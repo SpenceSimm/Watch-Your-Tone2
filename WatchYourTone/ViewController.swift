b1fe92516cb482838895f79a187b4f6aecd3062a
Simmons, Spence D <130499@s020rmc148-04.local>
2019-04-23 10:10:38 -0500

    Initial Commit

---
+++ WatchYourTone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WatchYourTone
+//
+//  Created by Simmons, Spence D on 4/23/19.
+//  Copyright © 2019 Simmons, Spence D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
