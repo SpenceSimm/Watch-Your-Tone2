f5af4d0968367daa84f23fc9ef782a10984ff258
Simmons, Spence D <spence.simmons.499@k12.friscoisd.org>
2019-04-25 09:30:31 -0500

    Added AudioRecordingViewController

---
+++ WatchYourTone/AudioRecordingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AudioRecordingViewController.swift
+//  WatchYourTone
+//
+//  Created by Simmons, Spence D on 4/25/19.
+//  Copyright © 2019 Simmons, Spence D. All rights reserved.
+//
+
+import UIKit
+
+class AudioRecordingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
