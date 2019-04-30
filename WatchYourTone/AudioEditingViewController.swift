267793670d91426228104d1b80e9970da470334c
milanp129 <milanp129@gmail.com>
2019-04-30 09:54:49 -0500

    added slider

--- WatchYourTone/AudioEditingViewController.swift
+++ WatchYourTone/AudioEditingViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class AudioEditingViewController: UIViewController {
 
+    @IBOutlet weak var Pitch: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
