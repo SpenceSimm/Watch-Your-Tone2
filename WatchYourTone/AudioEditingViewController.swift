79c6cbfa37f0de68adbb62a29f6d7728bcdb6e2c
milanp129 <milanp129@gmail.com>
2019-05-02 09:21:10 -0500

    added editing screen

--- WatchYourTone/AudioEditingViewController.swift
+++ WatchYourTone/AudioEditingViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import AVKit
 
 class AudioEditingViewController: UIViewController {
 
-    @IBOutlet weak var Pitch: UISlider!
+    var Pitch:Int = 0
+    var Volume:Int = 0
+    var Speed:Int = 0
+    @IBAction func pitchChanged(_ sender: Any) {
+    }
+    @IBAction func volumeChanged(_ sender: Any) {
+    }
+    @IBAction func speedChanged(_ sender: Any) {
+    }
+    
+    let engine = AVAudioEngine()
+    let speedControl = AVAudioUnitVarispeed()
+    let pitchControl = AVAudioUnitTimePitch()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +31,30 @@ class AudioEditingViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func play(_ url: URL) throws {
+        // 1: load the file
+        let file = try AVAudioFile(forReading: url)
+        
+        // 2: create the audio player
+        let audioPlayer = AVAudioPlayerNode()
+        
+        // 3: connect the components to our playback engine
+        engine.attach(audioPlayer)
+        engine.attach(pitchControl)
+        engine.attach(speedControl)
+        
+        // 4: arrange the parts so that output from one is input to another
+        engine.connect(audioPlayer, to: speedControl, format: nil)
+        engine.connect(speedControl, to: pitchControl, format: nil)
+        engine.connect(pitchControl, to: engine.mainMixerNode, format: nil)
+        
+        // 5: prepare the player to play its file from the beginning
+        audioPlayer.scheduleFile(file, at: nil)
+        
+        // 6: start the engine and player
+        try engine.start()
+        audioPlayer.play()
+    }
 
 
 }
